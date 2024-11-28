71a2d45b0d362f54fcc11b31c9d4cd8475a1a01b
Lauren <46947791+LaurenC2022@users.noreply.github.com>
2024-11-28 04:13:55 +0000

    update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,10 @@
 # $ deactivate 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
