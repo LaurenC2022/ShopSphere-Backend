8ad1a65d74cdb8e75e7bc530f424de20961834d3
Lauren <46947791+LaurenC2022@users.noreply.github.com>
2024-11-28 03:51:34 +0000

    update usage info

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
+# usage: 
+# $ . venv/.venv/bin/activate
+# $ pip install requirements.txt
+# $ cd app
+# $ flask --app __init__ run
+# quit deploy
+# $ ctl c 
+# deactivates venv 
+# $ deactivate 
+
 from flask import Flask
 
 app = Flask(__name__)
