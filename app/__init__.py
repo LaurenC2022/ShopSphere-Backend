3fdd617d6e43460497530bcc1bd3b3cddede9307
madefromjames <madefromjames@gmail.com>
2024-12-12 05:27:54 +0100

    add Babel support and update requirements for localization

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,26 @@
 # deactivates venv 
 # $ deactivate 
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_babel import Babel
 
 app = Flask(__name__)
+
+# Babel configuration
+app.config['BABEL_DEFAULT_LOCALE'] = 'en' # default language
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'es', 'fr'] # languages supported
+
+def get_locale():
+    return request.accept_languages.best_match(['en', 'es', 'fr'])
+
+# Initialize extensions
+"""You can add other extensions here, e.g. db = SQLAlchemy(app)"""
+babel = Babel(app, locale_selector=get_locale)
+
 
 @app.route("/")
 def hello_world():
